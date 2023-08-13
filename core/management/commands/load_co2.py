73c234e90cbba1d817215acc976939b4911f782d
alexdotzip <ajpurpora@gmail.com>
2023-08-12 22:32:01 -0400

    initial commit

--- core/management/commands/load_co2.py
+++ core/management/commands/load_co2.py
@@ -1,3 +1,9 @@
+import csv
+from datetime import date
+from itertools import islice
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from core.models import Rokkr
 from django.core.management.base import BaseCommand
 
 
@@ -5,4 +11,8 @@ class Command(BaseCommand):
     help = 'Load data from CO2 file'
 
     def handle(self, *args, **kwargs):
+        datafile = settings.BASE_DIR / 'data' / 'co2_mm_mlo.csv'
+
+        with open(datafile, 'r' as csvfile):
+            reader = csv.DictReader(csvfile)
         raise NotImplementedError
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Rokkr(models.Model):
+    date = models.DateField()
+    mapplayed = models.CharField(max_length=25)
+    opponent = models.CharField(max_length=3)
+    p1 = models.FloatField()
+    p2 = models.FloatField()
+    p3 = models.FloatField()
+    p4 = models.FloatField()
+    p5 = models.FloatField()
+
+    class Meta:
+        ordering = ('date',)
